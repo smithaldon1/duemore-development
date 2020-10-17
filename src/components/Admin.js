473972f53a84e125a3a4088ab733cb7fad9fb982
Aldon Smith <asmith@smithbros.dev>
2020-10-16 21:00:45 -0400

    Finished

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,15 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-import * as ROLES from '../constants/roles';
-import { withAuthorization } from '../session';
+import { withFirebase } from '../services';
 
-const AdminPage = () => (
-    <div>
-        <h1>Admin Page</h1>
-        <p>Restricted Area! Only users with the admin role are authorized.</p>
-    </div>
-);
+class AdminPage extends Component {
+    constructor(props) {
+        super(props);
 
-const condition = authUser => authUser && !!authUser.roles[ROLES.ADMIN];
+        this.state = {
+            loading: false,
+            users: [],
+        };
+    }
 
-export default withAuthorization(condition)(AdminPage);
+    componentDidMount() {
+        this.setState({ loading: true });
+
+        this.props.firebase.users().on('value', snapshot => {
+            const usersObject = snapshot.val();
+
+            const usersList = Object.keys(usersObject).map(key => ({
+                ...usersObject[key],
+                uid: key,
+            }));
+
+            this.setState({
+                users: usersList,
+                loading: false,
+            });
+        });
+    }
+
+    componentWillUnmount() {
+        this.props.firebase.users().off();
+    }
+
+    render() {
+        const { users, loading } = this.state;
+
+        return(
+            <div>
+                <h1>Admin</h1>
+
+                {loading && <div>Loading...</div>}
+                
+                <UserList users={users} />
+            </div>
+        );
+    }
+}
+
+const UserList = ({ users }) => (
+    <ul>
+        {users.map(user => (
+            <li key={user.uid}>
+                <span>
+                    <strong>ID:</strong> {user.uid}
+                </span>
+                <span>
+                    <strong>E-Mail:</strong> {user.email}
+                </span>
+                <span>
+                    <strong>Username:</strong> {user.username}
+                </span>
+            </li>
+        ))}
+    </ul>
+)
+
+export default withFirebase(AdminPage);
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,6 +1,6 @@
 import app from 'firebase/app';
 import 'firebase/auth';
-import 'firebase/firestore'
+import 'firebase/database';
 
 const config = {
     apiKey: process.env.REACT_APP_API_KEY,
@@ -16,7 +16,7 @@ class Firebase {
         app.initializeApp(config);
 
         this.auth = app.auth();
-        this.db = app.firestore;
+        this.db = app.database();
     }
 
     // *** Auth API ***
@@ -33,7 +33,9 @@ class Firebase {
 
     // *** User API ***
 
-    
+    user = uid => this.db.ref(`users/${uid}`);
+
+    users = () => this.db.ref('users');
 }
 
 export default Firebase;
